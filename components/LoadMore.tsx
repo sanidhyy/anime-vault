1d590c157912efc05e9c495718df1e825947e9f8
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-25 17:29:44 +0000

    loaded more anime feature added

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,10 +1,33 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+
+import AnimeCard, { AnimeProp } from "./AnimeCard";
+import { fetchAnime } from "@/app/action";
 
 function LoadMore() {
+  const { ref, inView } = useInView();
+  const [data, setData] = useState<AnimeProp[]>([]);
+
+  useEffect(() => {
+    if (inView) {
+      fetchAnime(2).then((res) => {
+        setData([...data, ...res]);
+      });
+    }
+  }, [inView, data]);
+
   return (
     <>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data.map((item: AnimeProp, index) => (
+          <AnimeCard key={item.id} anime={item} index={index} />
+        ))}
+      </section>
       <section className="flex justify-center items-center w-full">
-        <div>
+        <div ref={ref}>
           <Image
             src="./spinner.svg"
             alt="spinner"
